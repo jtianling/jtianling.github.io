a9514ece888b8a6332bee5277546249211131734
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2014-03-16 18:46:15 +0200

    Rebuild site

--- merge_source.sh
+++ merge_source.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+git merge source
+git filter-branch -f --subdirectory-filter build -- master
+
 touch README.md
 echo "This is my personal blog buit using [Metalsmith](http://www.metalsmith.io)" > README.md
